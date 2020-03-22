0393f7c23480a12dc632bbfc74df67bfec2a720e
DinaTaklit <ez_taklit@esi.dz>
2020-03-22 03:22:00 +0100

    feat: add intro
        - fix out the css of itro and topbar navbar

---
+++ assets/js/oxbridge.js
@@ -0,0 +1,13 @@
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    /*--/ Star Typed /--*/
+    new TypeIt('.text-slider-items', {
+
+        strings: ["Société De Conseil", "&", "De Développement Informatique"],
+        typeSpeed: 2,
+        cursor: false  
+      }).go();
+
+})(jQuery); // End of use strict
