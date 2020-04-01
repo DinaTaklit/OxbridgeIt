2ed173d1acf7a98965d2456aa9066a3f7e64df32
DinaTaklit <ez_taklit@esi.dz>
2020-04-01 20:22:30 +0100

    refact: remove js commented code

--- assets/js/oxbridge.js
+++ assets/js/oxbridge.js
@@ -70,39 +70,6 @@
   });
 
 
-
-
-    /*--/ Star Typed /--*/
-    // new TypeIt('.text-slider-items', {
-
-    //     strings: ["Société De Conseil", "&", "De Développement Informatique"],
-    //     typeSpeed: 2,
-    //     cursor: false  
-    //   }).go();
-      
-      // Check the window on resize to fix the navbar in top 
-      // $(window).on('resize', function(){
-      //   var win = $(this); //this = window
-      //   if (win.width() <= 800) {
-      //     $("#mainNav").css({"top": "0"});
-      //   }
-      //   else 
-      //     if ($(window).scrollTop()> $('#topbar').height()/6)
-      //         $("#mainNav").css({"top": "0vh"});
-      //     else
-      //         $("#mainNav").css({"top": "5vh"});
-      // });
-      // Add the fixed top after a scroll 
-    //   $(window).scroll(function (event) {
-    //     var scroll = $(window).scrollTop();
-    //     var topbar_height = $('#topbar').height()/6;
-    //     if(scroll>topbar_height)
-    //       $("#mainNav").css({"top": "0"});
-    //     else 
-    //       $("#mainNav").css({"top": "5vh"});
-        
-    // });
-
       // Smooth scrolling using jQuery easing
       $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -117,21 +84,6 @@
         }
     });
 
-      // Closes responsive menu when a scroll trigger link is clicked
-    //   $('.js-scroll-trigger').click(function() {
-    //       $('.navbar-collapse').collapse('hide');
-    //   });
-
-    //   // Collapse Navbar
-    //   var navbarCollapse = function() {
-    //     if ($("#mainNav").offset().top > 100) {
-    //         $("#mainNav").addClass("navbar-shrink");
-    //     } else {
-    //         $("#mainNav").removeClass("navbar-shrink");
-    //     }
-    // };
-    //  // Collapse now if page is not at top
-    // navbarCollapse();
     AOS.init();
     
   // Counter up 
